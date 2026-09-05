e783cb46da1cdb63f34601f99054be22d218c4fe
Debanshu2005 <debanshusarkar187@gmail.com>
2026-09-05 15:13:55 +0530

    Fix MAVSDK crash by preventing 20Hz spam of one-shot commands

--- DroneOS2/core/flight_pipeline.py
+++ DroneOS2/core/flight_pipeline.py
@@ -60,11 +60,27 @@ class CommandWriter:
     def __init__(self, fc: IFlightController):
         self.fc = fc
         self.last_action_time = 0.0
+        self.last_action_type = None
 
     async def execute(self, intent: FlightIntent):
         self.last_action_time = time.time()
         
         try:
+            # Prevent spamming one-shot commands at 20Hz
+            is_oneshot = intent.action in [
+                IntentAction.HOVER, 
+                IntentAction.LAND, 
+                IntentAction.RTL, 
+                IntentAction.TAKEOFF, 
+                IntentAction.EMERGENCY_KILL,
+                IntentAction.GOTO,
+                IntentAction.GOTO_NED
+            ]
+            if is_oneshot and self.last_action_type == intent.action:
+                return
+                
+            self.last_action_type = intent.action
+
             if intent.action == IntentAction.EMERGENCY_KILL:
                 if hasattr(self.fc, 'kill'):
                     await self.fc.kill()
